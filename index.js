b6d2d647aecb1f9b4b932c0e73bfaf4cce202750
kennyhei <kenny_heinonen@hotmail.com>
2018-01-15 20:41:01 +0200

    Setup heroku

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const middleware = require('./utils/middleware')
 
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 app.use(middleware.tokenExtractor)
